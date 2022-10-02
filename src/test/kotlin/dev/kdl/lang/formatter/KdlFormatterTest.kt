6cdec651f17cf23728e57df5ac3c19cc8e74a2cd
Exidex <16986685+Exidex@users.noreply.github.com>
2022-10-02 21:02:17 +0200

    Add base case test for formatter

---
+++ src/test/kotlin/dev/kdl/lang/formatter/KdlFormatterTest.kt
@@ -0,0 +1,16 @@
+package dev.kdl.lang.formatter
+
+import com.intellij.psi.formatter.FormatterTestCase
+import com.intellij.testFramework.TestDataPath
+import org.junit.Assert.*
+
+
+@TestDataPath("\$CONTENT_ROOT/testData")
+class KdlFormatterTest : FormatterTestCase() {
+    override fun getTestDataPath(): String = "src/test/testData"
+    override fun getBasePath(): String = "formatter"
+
+    override fun getFileExtension(): String = "kdl"
+
+    fun test1() = doTest()
+}
